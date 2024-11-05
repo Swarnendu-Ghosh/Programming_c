45bf46339886d4ade073800eed410a6131b94a2b
Swarnendu009 <swaarnendughosh@gmail.com>
2024-11-05 22:25:42 +0530

    Update

--- C/P-24.c
+++ C/P-24.c
@@ -5,26 +5,28 @@
 //60 to 70:B
 //45 to 60:C
 //<45:D
-//#include<stdio.h>
-//int main()
-//{
-//int marks;
-//printf("Enter Marks:\n");
-//scanf("%d",&marks);
-//if((marks>100)||(marks<0)
-//printf("WRONG ENTRY TRY AGAIN");
-//else if((marks<=100)&&(marks>=95))
-//printf("Grade is:AA");
-//else if((marks<95)&&(marks>=85))
-//printf("Grade is:A+");
-//else if((marks<85)&&(marks>=70))
-//printf("Grade is:A");
-//else if((marks<70)&&(marks>=60))
-//printf("Grade is:B");
-//else if((marks<60)&&(marks>=45))
-//printf("Grade is:C");
-//else
-//printf("Grade is:D");
-//}
+#include <stdio.h>
 
+int main() {
+    int marks;
+    printf("Enter Marks:\n");
+    scanf("%d", &marks);
+    
+    if ((marks > 100) || (marks < 0)) {
+        printf("WRONG ENTRY. TRY AGAIN.\n");
+    } else if ((marks <= 100) && (marks >= 95)) {
+        printf("Grade is: O\n");
+    } else if ((marks < 95) && (marks >= 85)) {
+        printf("Grade is: A+\n");
+    } else if ((marks < 85) && (marks >= 70)) {
+        printf("Grade is: A\n");
+    } else if ((marks < 70) && (marks >= 60)) {
+        printf("Grade is: B\n");
+    } else if ((marks < 60) && (marks >= 45)) {
+        printf("Grade is: C\n");
+    } else {
+        printf("Grade is: D\n");
+    }
 
+    return 0;
+}
