f52e95efb86ca55b832cfc344098acc2bd29d0cc
Swarnendu009 <swaarnendughosh@gmail.com>
2024-11-07 22:09:38 +0530

    Update

--- C/P-33.c
+++ C/P-33.c
@@ -2,13 +2,12 @@
 #include<stdio.h>
 int main()
 {
-char ch;
-int lc,uc;
-printf("Enter a Character:\n");
-scanf("%c",&ch);
-if(ch>='A'&&ch<='Z')
-printf("The character entered is in uppercase");
-else
-printf("The character entered is in lowercase");
+    char ch;
+    int lc,uc;
+    printf("Enter a Character:\n");
+    scanf("%c",&ch);
+    if(ch>='A'&&ch<='Z')
+    printf("The character entered is in uppercase");
+    else
+    printf("The character entered is in lowercase");
 }
- 
