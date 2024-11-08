e1e03c175fcc0dc67c83bc453b6af2fed69f4ed8
Swarnendu009 <swaarnendughosh@gmail.com>
2024-11-08 17:59:37 +0530

    Update

--- C/P-37.c
+++ C/P-37.c
@@ -2,15 +2,15 @@
 #include<stdio.h>
 int main()
 {
-int a,b,c,d;
-printf("Enter the numbers:\n");
-scanf("%d%d%d%d",&a,&b,&c,&d);
-if((a<b)&&(a<c)&&(a<d))
-printf("The smallest number is:%d",a);
-if((b<a)&&(b<c)&&(b<d))
-printf("The smallest number is:%d",b);
-if((c<a)&&(c<b)&&(c<d))
-printf("The smallest number is:%d",c);
-if((d<a)&&(d<b)&&(d<c))
-printf("The smallest number is:%d",d);
+    int a,b,c,d;
+    printf("Enter the numbers:\n");
+    scanf("%d%d%d%d",&a,&b,&c,&d);
+    if((a<b)&&(a<c)&&(a<d))
+    printf("The smallest number is:%d",a);
+    if((b<a)&&(b<c)&&(b<d))
+    printf("The smallest number is:%d",b);
+    if((c<a)&&(c<b)&&(c<d))
+    printf("The smallest number is:%d",c);
+    if((d<a)&&(d<b)&&(d<c))
+    printf("The smallest number is:%d",d);
 }
