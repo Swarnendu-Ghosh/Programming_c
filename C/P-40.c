eef382650fd0af06020705568093dce053c37e4b
Swarnendu009 <swaarnendughosh@gmail.com>
2024-11-09 20:33:13 +0530

    Update

--- C/P-40.c
+++ C/P-40.c
@@ -2,27 +2,27 @@
 #include<stdio.h>
 int main()
 {
-char a;
-printf("Enter character:");
-scanf("%c",&a);
-switch(a)
-{
-case 'a':
-printf("It is A");
-break;
-case 'e':
-printf("It is E");
-break;
-case 'i':
-printf("It is I");
-break;
-case 'o':
-printf("It is O");
-break;
-case 'u':
-printf("It is U");
-break;
-default:
-printf("It is a Consonant");
-}
+    char a;
+    printf("Enter character:");
+    scanf("%c",&a);
+    switch(a)
+    {
+        case 'a':
+        printf("It is A");
+        break;
+        case 'e':
+        printf("It is E");
+        break;
+        case 'i':
+        printf("It is I");
+        break;
+        case 'o':
+        printf("It is O");
+        break;
+        case 'u':
+        printf("It is U");
+        break;
+        default:
+        printf("It is a Consonant");
+    }
 }
