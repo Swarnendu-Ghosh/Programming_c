717063bbd26f06a51b2828c942e516fd283b5567
Swarnendu009 <swaarnendughosh@gmail.com>
2024-11-10 22:50:26 +0530

    Update

--- C/P-44.c
+++ C/P-44.c
@@ -2,13 +2,13 @@
 #include<stdio.h>
 int main()
 {
-int a=1,n;
-printf("Enter a number:");
-scanf("%d",&n);
-while(n>0)
-{
-a*=n;
-n--;
-}
-printf("The factorial of the number %d is:%d",n,a);
+    int a=1,n;
+    printf("Enter a number:");
+    scanf("%d",&n);
+    while(n>0)
+    {
+        a*=n;
+        n--;
+    }
+    printf("The factorial of the number %d is:%d",n,a);
 }
